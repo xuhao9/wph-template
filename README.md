275156d29cf1cdbdb5aa96e08251aa5ad6e465d5
xuhao <295830672@qq.com>
2020-04-08 15:08:34 +0800

    项目gulp调整

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 脚手架工具模版
+// npm run dev启动项目
+// 使用npm run build会打包到dist文件夹,
+// css和html内的图片地址会自动替换为cdn地址
+// html内的img标签写在同一行,不要换行
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,43 @@
+// npm run dev启动项目
+// 使用npm run build会打包到dist文件夹,
+// css和html内的图片地址会自动替换为cdn地址
+// html内的img标签写在同一行,不要换行
+
+// 获取当前文件夹名
+const arr = __dirname.split('/');
+const dir = arr[arr.length - 1];
+// CDN地址
+const url = `https://m-1254038939.cos.ap-guangzhou.myqcloud.com/${dir}/`;
+console.log(url);
 const gulp = require('gulp'),
     minifycss = require('gulp-minify-css'), //css压缩
     concat = require('gulp-concat'), //合并文件
     uglify = require('gulp-uglify'), //js压缩
+    imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'), //文件重命名
     del = require('del'),
-    notify = require('gulp-notify'),
+    notify = require('gulp-notify'), //提示
     babel = require('gulp-babel'),
-    gutil = require('gutil'), //提示
+    gutil = require('gutil'),
     connect = require('gulp-connect'),
-    cp = require('child_process');
+    cp = require('child_process'),
+    htmlreplace = require('gulp-html-replace'),
+    htmlmini = require('gulp-htmlmin'),
+    tap = require('gulp-tap');
 
 // css
+
+const regCss = /url\((.*?)\/img\/(.*?)\)/g;
 gulp.task('minifycss',function(){
     return gulp.src('css/*.css')      //设置css
+        .pipe(tap(file => {
+            const contents = file.contents.toString().replace(regCss, 'url(' + url + 'img/' + '$2' + ')')
+            file.contents = Buffer.from(contents)
+        }))
         .pipe(concat('index.css'))      //合并css文件到"index"
         .pipe(rename({suffix:'.min'}))         //修改文件名
         .pipe(minifycss())                    //压缩文件
-        .pipe(gulp.dest('css/min'))           //设置输出路径
+        .pipe(gulp.dest('dist/css'))           //设置输出路径
         .pipe(notify({message:'css task ok'}));   //提示成功
 });
 
@@ -26,7 +47,7 @@ gulp.task('minifyjs',function(){
         .pipe(babel({
             presets: ['es2015'] // es5检查机制
         }))
-        .pipe(concat('bunld.js')) //合并js
+        .pipe(concat('bundle.js')) //合并js
         .pipe(rename({
             suffix: '.min'
         })) //重命名
@@ -34,9 +55,44 @@ gulp.task('minifyjs',function(){
         .on('error', function (err) {
             gutil.log(err.toString());
         })
-        .pipe(gulp.dest('js/min')) //输出
+        .pipe(gulp.dest('dist/js')) //输出
         .pipe(notify({message:"js task ok"}));    //提示
 });
+// img压缩
+gulp.task('imgtask',function(){
+    return gulp.src(['img/*.{jpg,png}', 'img/*/*.{png,jpg}'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
+        .pipe(notify({message:"img task ok"}));    //提示
+});
+// html资源替换+压缩
+const regHtml = /<img(.*?)src=['|"](.*?)img\/(.*?)['|"](.*?)>/g;
+gulp.task('htmltask',function() {
+    return gulp.src('index.html')
+            .pipe(tap(file => {
+                const newContent = `<img$1src="${url}img/$3"$4/>`;
+                const contents = file.contents.toString().replace(regHtml, newContent);
+                file.contents = Buffer.from(contents);
+            }))
+            .pipe(htmlreplace({
+                'css': url + 'css/index.min.css',
+                'js': url + 'js/bundle.min.js'
+            }))
+            .pipe(htmlmini({
+                removeComments: true, // 去除html注释
+                collapseWhitespace: true // 压缩html
+            }))
+            .pipe(gulp.dest('dist'))
+            .pipe(notify({message:"html task ok"}));    //提示
+});
+
+// 清除原文件
+gulp.task('clean', function(cb) {
+    del('dist/*', cb)
+});
+// 生成打包任务
+gulp.task('default', ['clean', 'minifyjs', 'minifycss', 'htmltask', 'imgtask']);
+
 const platform = {
     'wind32': 'start',
     'linux': 'xdg-open',
@@ -62,11 +118,5 @@ gulp.task('reload',function() {
     gulp.src(['js/*.js', 'css/*.css', 'index.html'])
     .pipe(connect.reload());
 });
-// 清除原文件
-gulp.task('clean', function(cb) {
-    del(['css/min', 'js/min'], cb)
-});
-console.log(process.argv);
-gulp.task('default', ['webserver', 'clean', 'minifyjs', 'minifycss', 'watch'], function () {
-    
-});
+// 开发启动任务
+gulp.task('dev', ['webserver', 'watch'])
